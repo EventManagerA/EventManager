b4b5193de2c08d41af58cb62066eadf3e7bfc8e8
takahashi1993 <fmb07miwa@gmai.com>
2016-05-20 13:34:05 +0900

    view/group

---
+++ application/views/group/group_index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+
+</head>
+<body>
+<h1>グループ一覧</h1>
+
+<table border="1">
+<tr>
+   <th>会員ID</th>
+   <th>グループ名</th>
+</tr>
+<?php foreach ($groups as  $grouplist):?>
+<tr>
+	<td><?php echo $grouplist->id;?></td>
+	<td><?php echo $grouplist->name;?></td>
+</tr>
+<?php endforeach;?>
+</table>
+<p>
+	<a href="<?php echo base_url('group/add');?>">グループの登録</a>
+
+
+
+</body>
+</html>
