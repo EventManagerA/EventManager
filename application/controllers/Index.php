02d6a7a46a547c0fd9095141ffce9508f70960f6
Hamamichi <s.hmmc1641@gmail.com>
2016-05-20 15:09:19 +0900

    Index.php 途中

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,29 +1,44 @@
 <?php
 class Index extends CI_Controller {
 
-
-
 	public function login() {
-		$this-> load-> view('header');
-		$this-> load-> view('index/login');
-		$this-> load-> model('Users_model');
 
-		if($this-> input->post('login')) {
-			//認証成功 IDとパスのデータがあったら
+		$this-> load-> model('Users_model');
+		//ログイン済みだったらイベントへ
+		if($this-> session-> userdata('auth') === TRUE){
+			if($this-> session-> userdata('user') === TRUE){
+				redirect('event/index');
+			}
+		}
+		//postされたら
+		if($this-> input-> post('login')) {
+			//データ取得
+			$login_id = $this-> load-> post('login_id');
+			$login_pass = $this-> load-> post('login_pass');
+			//認証成功 ログインIDとpassのデータがあったら
 			if($this-> Users_model-> auth() === TRUE){
-				$this-> load->view('event/index');
+				//ユーザIDと名前取得
+				$data['name'] = $this-> Users_model->  get_row();
+
+				//セッションにidとauth登録
+				redirect('event/index');
 			}
 			//認証失敗
 			else {
 				redirect('index/login');
 			}
-
-
+		}
+		//postがなかったら（初期表示）
+		else{
+			$this-> load-> view('header');
+			$this-> load-> view('index/login');
 		}
 
 	}
 
 	public function logout() {
+		//セッション削除
+
 		$this-> load-> view('header');
 		$this-> load-> view('index/logout');
 
--- application/views/index/login.php
+++ application/views/index/login.php
@@ -10,8 +10,8 @@
 
 ?>
 
-<p><?php echo form_input('loginID','','placeholder="ログインID"'); ?></p>
-<p><?php echo form_input('pass','','placeholder="パスワード"'); ?></p>
+<p><?php echo form_input('login_id','','placeholder="ログインID"'); ?></p>
+<p><?php echo form_input('login_pass','','placeholder="パスワード"'); ?></p>
 <p><?php echo form_button('login', 'ログイン', 'size=100%'); ?></p>
 
 <?php echo form_close();?>
