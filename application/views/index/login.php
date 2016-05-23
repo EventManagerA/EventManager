511581372fc30bbd4b2f045daeb5231426b79998
Hamamichi <s.hmmc1641@gmail.com>
2016-05-23 16:00:01 +0900

    init

--- application/views/index/login.php
+++ application/views/index/login.php
@@ -1,7 +1,5 @@
 <?php echo form_open(); ?>
-
-<p><?php echo form_input('login_id','','placeholder="ログインID"'); ?></p>
-<p><?php echo form_password('login_pass','','placeholder="パスワード"'); ?></p>
-<p><?php echo form_submit('login_submit', 'ログイン'); ?></p>
-
+<p><?php echo form_input('login_id','','placeholder="ログインID" class="form-control"'); ?></p>
+<p><?php echo form_password('login_pass','','placeholder="パスワード" class="form-control"'); ?></p>
+<p><?php echo form_submit('login_submit', 'ログイン', "class='btn btn-block'"); ?></p>
 <?php echo form_close(); ?>
