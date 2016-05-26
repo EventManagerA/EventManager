f4d818097c38ad545b6a9e54755b6482e831826d
k-suzuki-p <ks15124037@gmail.com>
2016-05-26 15:13:39 +0900

    st_valueとgetter

--- application/views/event/edit.php
+++ application/views/event/edit.php
@@ -3,32 +3,32 @@
 <div class="form-group">
 	<?php echo form_label('タイトル(必須)', 'InputTitle');?>
 	<?php echo form_error('title','<p class="text-danger">','</p>')?>
-	<?php echo form_input(['name'=>'title','class'=>'form-control','id'=>'InputTitle','value'=>$event_row->get_title()])?>
+	<?php echo form_input(['name'=>'title','class'=>'form-control','id'=>'InputTitle','value'=>set_value('title',$event_row->get_title())])?>
 </div>
 <div class="form-group">
 	<?php echo form_label('開始日時(必須)', 'InputStart');?>
 	<?php echo form_error('start','<p class="text-danger">','</p>')?>
-	<?php echo form_input(['name'=>'start','class'=>'form-control','id'=>'InputStart','placeholder'=>'0000-00-00 00:00:00','value'=>$event_row->get_start()])?>
+	<?php echo form_input(['name'=>'start','class'=>'form-control','id'=>'InputStart','placeholder'=>'0000-00-00 00:00:00','value'=>set_value('start',$event_row->get_start())])?>
 </div>
 <div class="form-group">
 	<?php echo form_label('終了日時(必須)', 'InputEnd');?>
 	<?php echo form_error('end','<p class="text-danger">','</p>')?>
-	<?php echo form_input(['name'=>'end','class'=>'form-control','id'=>'InputEnd','placeholder'=>'0000-00-00 00:00:00','value'=>$event_row->get_end()])?>
+	<?php echo form_input(['name'=>'end','class'=>'form-control','id'=>'InputEnd','placeholder'=>'0000-00-00 00:00:00','value'=>set_value('end',$event_row->get_end())])?>
 </div>
 <div class="form-group">
 	<?php echo form_label('場所(必須)', 'InputPlace');?>
 	<?php echo form_error('place','<p class="text-danger">','</p>')?>
-	<?php echo form_input(['name'=>'place','class'=>'form-control','id'=>'InputPlace','value'=>$event_row->get_place()])?>
+	<?php echo form_input(['name'=>'place','class'=>'form-control','id'=>'InputPlace','value'=>set_value('place',$event_row->get_place())])?>
 </div>
 <div class="form-group">
 	<?php echo form_label('対象グループ(必須)', 'InputGroup');?>
 	<?php echo form_error('group','<p class="text-danger">','</p>')?>
-	<?php echo form_dropdown('group',$groupList,$event_row->get_group_id(),"class='form-control' id ='InputGroup'")?>
+	<?php echo form_dropdown('group',$groupList,set_value('group',$event_row->get_group_id()),"class='form-control' id ='InputGroup'")?>
 </div>
 <div class="form-group">
 	<?php echo form_label('詳細', 'InputDetail');?>
 	<?php echo form_error('detail','<p class="text-danger">','</p>')?>
-	<?php echo form_textarea(['name'=>'detail','class'=>'form-control','id'=>'InputDetail','value'=>$event_row->get_detail()])?>
+	<?php echo form_textarea(['name'=>'detail','class'=>'form-control','id'=>'InputDetail','value'=>set_value('detail',$event_row->get_detail())])?>
 </div>
 <p>
 	<?php echo form_submit(['name'=>'cancel','class'=>'btn btn-default','value'=>'キャンセル'])?>
--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -5,28 +5,28 @@
 <div class="form-group">
 	<?php echo form_label('氏名(必須)','InputName')?>
 	<?php echo form_error('name', '<p>', '</p>'); ?>
-	<?php echo form_input(['name'=>'name','class'=>'form-control','id'=>'InputName','value'=>set_value('name',$users->name)]) ?>
+	<?php echo form_input(['name'=>'name','class'=>'form-control','id'=>'InputName','value'=>set_value('name',$users->get_name())]) ?>
 	<span id="helpBlock" class="help-block">ひらがな、カタカナ、漢字、「・」が使用できます。   例）田中 太郎</span>
 </div>
 
 <div class="form-group">
 	<?php echo form_label('ログインID(必須)','InputId')?>
 	<?php echo form_error('login_id', '<p>', '</p>'); ?>
-	<?php echo form_input(['name'=>'login_id','class'=>'form-control','id'=>'InputId','value'=>set_value('InputId',$users->login_id)]) ?>
+	<?php echo form_input(['name'=>'login_id','class'=>'form-control','id'=>'InputId','value'=>set_value('login',$users->get_login_id())]) ?>
 	<span id="helpBlock" class="help-block">英数字、「-」「_」が使用できます。2字以上で記入してください。</span>
 </div>
 
 <div class="form-group">
 	<?php echo form_label('パスワード(変更の場合のみ)','InputPass')?>
 	<?php echo form_error('password', '<p>', '</p>'); ?>
-	<?php echo form_password(['name'=>'password','class'=>'form-control','id'=>'InputPass','placeholder'=>'パスワード','value'=>set_value('InputPass')]) ?>
+	<?php echo form_password(['name'=>'password','class'=>'form-control','id'=>'InputPass','placeholder'=>'パスワード','value'=>set_value('password')]) ?>
 	<span id="helpBlock" class="help-block">英数字、「-」「_」が使用できます。6字以上で記入してください。</span>
 </div>
 
 <div class="form-group">
 	<?php echo form_label('所属グループ(必須)','InputGroup')?>
 	<?php echo form_error('group', '<p>', '</p>'); ?>
-	<?php echo form_dropdown('group',$groupList,$users->group_id,"class='form-control' id ='InputGroup'")?>
+	<?php echo form_dropdown('group',$groupList,set_value('group',$users->get_group_id()),"class='form-control' id ='InputGroup'")?>
 	<span id="helpBlock" class="help-block">所属グループを選択してください。</span>
 </div>
 <!-- model / users で$usersの確認-->
