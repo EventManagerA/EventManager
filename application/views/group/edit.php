9098f2936f95687b72a5e6b9c7c423d9df68c82a
takahashi1993 <fmb07miwa@gmail.com>
2016-05-26 15:51:10 +0900

    表示色変更及び必須表記追加

--- application/views/group/edit.php
+++ application/views/group/edit.php
@@ -1,11 +1,12 @@
 
 <h1>部署編集</h1>
 <?php echo form_open();?>
-<p>
-       <?php echo form_label('部署名', 'InputName');?>
-        <?php echo form_error('name')?>
+<div class="form-group">
+       <?php echo form_label('部署名(必須)', 'InputName');?>
+        <?php echo form_error('name','<p class="text-danger">','</p>')?>
          <?php echo form_input(['name'=>'name','class'=>'form-control','id'=>'InputName','value'=>set_value('name',$group_row->get_name())])?>
 
+</div>
 <p>
 <?php echo form_submit(['name'=>'cancel','class'=>'btn btn-default','value'=>'キャンセル'])?>
 	<?php echo form_submit(['name'=>'add','class'=>'btn btn-primary','value'=>'保存'])?>
