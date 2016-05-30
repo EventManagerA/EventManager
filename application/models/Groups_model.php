81145b909783d5c1f0e7561d631d5f2dce6e79f1
k-suzuki-p <ks15124037@gmail.com>
2016-05-30 14:56:01 +0900

    ちょっとリファクト

--- application/models/Groups_model.php
+++ application/models/Groups_model.php
@@ -4,36 +4,41 @@ class Groups_model extends CI_Model {
 	public $id;
 	public $name;
 
-   public function get_rowset_desc($page ='',$perPage =''){
-   	{
-   		$this->db->order_by('id','desc');
-   		if(!$page){
-   			$page = 1;
-   		}
-   		if(!is_numeric($page)){
-   			$page = 1;
-   		}
-    		if (isset($page,$perPage))
-    		{
-   			$offset = ($page - 1) * $perPage;
-   			$query = $this->db->get('groups',$perPage,$offset);
-    		}else{
+	public function get_rowset_desc($page ='',$perPage =''){
 
-    			$query = $this->db->get('groups');
-    		}
+		$this->db->order_by('id','desc');
 
-   		return $query->result('Groups_model');
-   		}
-   }
+		if(!$page){
+			$page = 1;
+		}
 
-   public function total_count(){
-   	   return $this->db->count_all('groups');
-   	}
+		if(!is_numeric($page)){
+			$page = 1;
+		}
+
+ 		if (isset($page,$perPage))
+ 		{
+			$offset = ($page - 1) * $perPage;
+			$query = $this->db->get('groups',$perPage,$offset);
+ 		}else{
+
+ 			$query = $this->db->get('groups');
+ 		}
+
+		return $query->result('Groups_model');
+	}
+
+	public function total_count(){
+	   return $this->db->count_all('groups');
+	}
+
 	public function get_row_by_id($id)
 	{
 		$query = $this->db->get_where('groups', array('id' => $id));
 		return $query->row(0,'Groups_model');
 	}
+
+
 
 	public function update($id,$val){
 
@@ -51,6 +56,8 @@ class Groups_model extends CI_Model {
 	}
 
 
+
+//  idの取得
 	public function get_id() {
 		return isset($this->id) ? $this->id : false;
 	}
@@ -59,27 +66,30 @@ class Groups_model extends CI_Model {
 		return isset($this->name) ? $this->name : false;
 	}
 
+	//イベント登録時の部署リスト取得
 	public function get_list_for_form() {
 
 		$query = $this->db->get('groups');
-		$groupRowsetArray = $query->result_array();
+		$group_rowset_array = $query->result_array();
 
-		$groupList[''] = '全員';
-		foreach ($groupRowsetArray as $groupRowArray){
-			$groupList[$groupRowArray['id']] = $groupRowArray['name'];
+		$group_list[''] = '全員';
+		foreach ($group_rowset_array as $group_row_array){
+			$group_list[$group_row_array['id']] = $group_row_array['name'];
 		}
 
-		return $groupList;
+		return $group_list;
 	}
+
+	//ユーザ登録時の部署リスト取得
 	public function get_list_for_userform() {
 
 		$query = $this->db->get('groups');
-		$groupRowsetArray = $query->result_array();
+		$group_rowset_array = $query->result_array();
 
-		foreach ($groupRowsetArray as $groupRowArray){
-			$groupList[$groupRowArray['id']] = $groupRowArray['name'];
+		foreach ($group_rowset_array as $group_row_array){
+			$group_list[$group_row_array['id']] = $group_row_array['name'];
 		}
 
-		return $groupList;
+		return $group_list;
 	}
 }
