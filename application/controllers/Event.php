dd850661bfde3230d017bc59c249baf1f399906f
k-suzuki-p <ks15124037@gmail.com>
2016-05-30 15:10:39 +0900

    分岐ミス

--- application/controllers/Event.php
+++ application/controllers/Event.php
@@ -165,7 +165,7 @@ class Event extends CI_Controller {
 
 		$logged_in_user = $this->load->get_var('logged_in_user');
 
-		if(!($logged_in_user->is_admin_user() || $data['event_row']->get_registered_by == $logged_in_user->get_id())){
+		if(!($logged_in_user->is_admin_user() || $data['event_row']->get_registered_by() == $logged_in_user->get_id())){
 			redirect('event/index');
 		}
 
@@ -210,7 +210,7 @@ class Event extends CI_Controller {
 			show_404();
 		}
 		$logged_in_user = $this->load->get_var('logged_in_user');
-		if(!($logged_in_user->is_admin_user() || $data['event_row']->get_registered_by == $logged_in_user->get_id())){
+		if(!($logged_in_user->is_admin_user() || $data['event_row']->get_registered_by() == $logged_in_user->get_id())){
 			redirect('event/index');
 		}
 
